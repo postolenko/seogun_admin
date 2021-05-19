76a26052d36438736dbd403afcf24fdddbeccca9
postolenko <denis.postolenko@gmail.com>
2021-05-20 01:51:17 +0300

    Seo

---
+++ README.md
@@ -0,0 +1,4 @@
+# http://postolenko.github.io/seogun_admin/login.html
+# http://postolenko.github.io/seogun_admin/add_server.html
+# http://postolenko.github.io/seogun_admin/edit_server.html
+# http://postolenko.github.io/seogun_admin/edit_parser.html
---
+++ js/scripts.js
@@ -0,0 +1,63 @@
+var w = window,
+d = document,
+e = d.documentElement,
+g = d.getElementsByTagName('body')[0],
+bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
+
+
+$(window).load(function() {
+
+
+
+});
+
+$(window).resize(function() {
+
+
+
+});
+
+$(document).scroll(function() {
+
+
+
+});
+
+$(document).ready(function() {
+
+    $(".dropdown_title").on("click", function(e) {
+      e.preventDefault();
+      parent = $(this).closest(".dropdown_box");
+      if(parent.hasClass("active")) {
+        parent.removeClass("active");
+      } else {
+        $(".dropdown_box").removeClass("active");
+        parent.addClass("active");
+      }
+    });
+
+    $(".val").on("click", function(e) {
+      e.preventDefault();
+      parent = $(this).closest(".dropdown_box");
+      activeVal = parent.find(".active_val");
+      parent.find(".hide_val").val($(this).html());
+      activeVal.html($(this).html());
+    });
+
+    $(this).keydown(function(eventObject){
+      if (eventObject.which == 27) {
+        $(".dropdown_box").removeClass("active");
+      }
+    });
+
+    $(document).on("mouseup", function(e) {
+        e.preventDefault();
+        hide_element = $(".dropdown_box");
+        if (!hide_element.is(e.target)
+            && hide_element.has(e.target).length === 0
+            && hide_element.hasClass("active")) {
+          hide_element.removeClass("active")
+        }
+    });
+
+});
